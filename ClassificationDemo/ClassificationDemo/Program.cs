40bd72410a2dc6a6d22a2e836d7e08745c000d35
filipw <wojcieszyn@gmail.com>
2020-06-15 18:05:23 +0200

    clean up

--- ClassificationDemo/ClassificationDemo/Program.cs
+++ ClassificationDemo/ClassificationDemo/Program.cs
@@ -1,12 +1,9 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Classification;
-using Microsoft.CodeAnalysis.Completion;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Host.Mef;
-using Microsoft.CodeAnalysis.QuickInfo;
 using Microsoft.CodeAnalysis.Text;
 using System;
-using System.Composition.Hosting;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,17 +13,6 @@ namespace ClassificationDemo
     {
         async static Task Main(string[] args)
         {
-            // default assemblies are
-            //    "Microsoft.CodeAnalysis.Workspaces",
-            //    "Microsoft.CodeAnalysis.CSharp.Workspaces",
-            //    "Microsoft.CodeAnalysis.VisualBasic.Workspaces",
-            //    "Microsoft.CodeAnalysis.Features",
-            //    "Microsoft.CodeAnalysis.CSharp.Features",
-            //    "Microsoft.CodeAnalysis.VisualBasic.Features"
-            // http://source.roslyn.io/#Microsoft.CodeAnalysis.Workspaces/Workspace/Host/Mef/MefHostServices.cs,126
-            var host = MefHostServices.Create(MefHostServices.DefaultAssemblies);
-            var workspace = new AdhocWorkspace(host);
-
             var code = @"using System;
 
             public class MyClass
@@ -36,7 +22,12 @@ namespace ClassificationDemo
                 }
             }";
 
+            var host = MefHostServices.Create(MefHostServices.DefaultAssemblies);
+            var workspace = new AdhocWorkspace(host);
+
             var souceText = SourceText.From(code);
+
+            // with a project
             var projectInfo = ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Create(), "MyProject", "MyProject", LanguageNames.CSharp).
                 WithMetadataReferences(new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) });
             var project = workspace.AddProject(projectInfo);
@@ -44,11 +35,25 @@ namespace ClassificationDemo
 
             var classifiedSpans = await Classifier.GetClassifiedSpansAsync(document, new TextSpan(0, code.Length));
 
-            foreach (var classifiedSpan in classifiedSpans)
+            foreach (var classifiedSpan in classifiedSpans.Where(s => !ClassificationTypeNames.AdditiveTypeNames.Contains(s.ClassificationType)))
             {
                 var position = souceText.Lines.GetLinePositionSpan(classifiedSpan.TextSpan);
                 Console.WriteLine($"{souceText.ToString(classifiedSpan.TextSpan)} - {classifiedSpan.ClassificationType} - {position.Start}:{position.End}");
             }
+
+            // with semantic model
+            var syntaxTree = CSharpSyntaxTree.ParseText(souceText);
+            var compilation = CSharpCompilation.Create("Dummy").AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location)).AddSyntaxTrees(syntaxTree);
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+
+            classifiedSpans = Classifier.GetClassifiedSpans(semanticModel, new TextSpan(0, code.Length), workspace);
+
+            foreach (var classifiedSpan in classifiedSpans.Where(s => !ClassificationTypeNames.AdditiveTypeNames.Contains(s.ClassificationType)))
+                {
+                var position = souceText.Lines.GetLinePositionSpan(classifiedSpan.TextSpan);
+                Console.WriteLine($"{souceText.ToString(classifiedSpan.TextSpan)} - {classifiedSpan.ClassificationType} - {position.Start}:{position.End}");
+            }
+
             Console.ReadLine();
         }
     }
